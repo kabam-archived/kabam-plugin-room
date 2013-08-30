774859c6483391743b6100e0e4cb546f98b11113
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-08-31 00:08:10 +0400

    [feature] - start work

---
+++ index.js
@@ -0,0 +1,53 @@
+var slugify = require('slugify');
+
+exports.name = "kabamPluginRoom";
+
+exports.extendModel('groups',function(kernel){
+  var GroupsSchema = new kabam.mongoose.Schema({
+    'name': {
+      type: String,
+      trim: true,
+      unique: true,
+      index: true
+      },
+    'uri': {
+      type: String,
+      trim: true,
+      unique: true,
+      index: true,
+      default: function(){
+        return slugify(this.name);
+        }
+      },
+    'parent':{
+      '_id': mongoose.Schema.Types.ObjectId,
+      'name': String,
+      'uri': String
+    },
+    'description': String,
+    'members':[{
+      '_id': mongoose.Schema.Types.ObjectId,
+      'username': String,
+      'gravatar': String,
+      'firstName': String,
+      'lastName': String
+    }],
+    'admins':[{
+      '_id': mongoose.Schema.Types.ObjectId,
+      'username': String,
+      'gravatar': String,
+      'firstName': String,
+      'lastName': String
+    }]
+  });
+
+  GroupsSchema.methods.inviteAdmin = function(usernameOrEmailOrUserObject){};
+  GroupsSchema.methods.removeAdmin = function(usernameOrEmailOrUserObject){};
+
+  GroupsSchema.methods.inviteUser = function(usernameOrEmailOrUserObject){};
+  GroupsSchema.methods.removeUser = function(usernameOrEmailOrUserObject){};
+
+  GroupsSchema.methods.sendMessage = function(usernameOrEmailOrUserObject, message){};
+
+  return kabam.mongoConnection.model('groups', GroupsSchema);
+});
