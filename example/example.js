d290e7596c145d211b428fe49ef808ad1478aeca
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-09-05 23:08:11 +0400

    [feature] generator for test data

--- example/example.js
+++ example/example.js
@@ -1,4 +1,5 @@
-var Kabam = require('kabam-kernel');
+var Kabam = require('kabam-kernel'),
+  async = require('async');
 
 var kabam = Kabam({
   'hostUrl':'http://vvv.msk0.ru/'
@@ -10,4 +11,93 @@ kabam.usePlugin(require('kabam-plugin-welcome'));
 
 kabam.usePlugin(require('./../index.js'));
 
+kabam.extendRoutes(function(kernel){
+  kernel.app.get('/h_init',function(request,response){
+    if(request.user){
+        async.waterfall([
+          function(cb){
+            var num = Math.floor(10000*Math.random());
+            request.model.groups.create({
+              'name':'Test school '+num,
+              'uri':'test_school_'+num,
+              'descriptionPublic':'Description for public',
+              'descriptionForMembers':'Description for members',
+              'tier':1,
+              'members': [
+                {
+                  '_id':request.user._id,
+                  'username': request.user.username,
+                  'gravatar': request.user.gravatar,
+                  'firstName': request.user.firstName,
+                  'lastName': request.user.lastName,
+                  'role': 'admin'
+                }]
+            },
+            function(err, schoolCreated){
+              cb(err, schoolCreated);
+            });
+          },
+          function(school,cb){
+            var num = Math.floor(10000*Math.random());
+            request.model.groups.create({
+              'name':'Test course '+num,
+              'uri':'test_course_'+num,
+              'descriptionPublic':'Description for public',
+              'descriptionForMembers':'Description for members',
+              'school_id':school._id,
+              'tier':2,
+              'members': [
+                {
+                  '_id':request.user._id,
+                  'username': request.user.username,
+                  'gravatar': request.user.gravatar,
+                  'firstName': request.user.firstName,
+                  'lastName': request.user.lastName,
+                  'role': 'admin'
+                }]
+            },
+            function(err, courseCreated){
+              cb(err, school, courseCreated);
+            });
+          },
+          function(school,course,cb){
+
+            var num = Math.floor(10000*Math.random());
+            request.model.groups.create({
+              'name':'Test group '+num,
+              'uri':'test_group_'+num,
+              'descriptionPublic':'Description for public',
+              'descriptionForMembers':'Description for members',
+              'school_id':school._id,
+              'course_id':course._id,
+              'tier': 2,
+              'members': [
+                {
+                  '_id':request.user._id,
+                  'username': request.user.username,
+                  'gravatar': request.user.gravatar,
+                  'firstName': request.user.firstName,
+                  'lastName': request.user.lastName,
+                  'role': 'admin'
+                }]
+            },
+            function(err, groupCreated){
+              cb(err, school, course, groupCreated);
+            });
+          },
+          function(school,course,group,cb){
+            cb(null,{
+              'school':school,
+              'course':course,
+              'group':group
+            });
+          },
+        ],function(err,result){
+          response.json(result);
+        });
+    } else {
+      response.send(403);
+    }
+    });
+  });
 kabam.start();
