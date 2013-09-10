18f64320e41d6e2eb8ae00ea3c55c6dbf7dfb799
Anatoliy Ostroumov <ostroumov095@gmail.com>
2013-09-10 16:55:10 +0400

    [test] - placeholder for tests

---
+++ test/groups.test.js
@@ -0,0 +1,9 @@
+var should = require('should'),
+  KabamKernel = require('kabam-kernel'),
+//  request = require('request'),
+  port = 3020;
+
+
+describe('kabam groups test',function(){
+  it('have to be created');
+});
